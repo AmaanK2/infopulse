e2203f5cb10f033aaf7708a669f8fcadcf7d3ece
AmaanK2 <khakianiamaan@gmail.com>
2024-03-16 13:05:05 -0400

    remove non articles from csv

--- infopulse.py
+++ infopulse.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import re  # Import the regular expressions library
 
 # URL of the site to scrape
 url = 'https://www.cbc.ca/'
@@ -10,14 +11,16 @@ soup = BeautifulSoup(response.text, 'html.parser')
 # Find all links that might lead to news articles
 articles = soup.find_all('a', href=True)
 
+# Regular expression to match links that end with a sequence of digits
+link_pattern = re.compile(r'\d+$')
+
 news = []
 for article in articles:
     if '/news/' in article['href']:
         link = 'https://www.cbc.ca' + article['href'] if not article['href'].startswith('http') else article['href']
-        if link not in [n[4] for n in news]:
+        if link_pattern.search(article['href']) and link not in [n[4] for n in news]:  # Check if link ends with number
             article_page = requests.get(link)
             article_soup = BeautifulSoup(article_page.text, 'html.parser')
-            print(article_soup)
             title = article_soup.find('h1').text.strip() if article_soup.find('h1') else 'N/A'
             category_element = article_soup.find('span', {'class': 'sclt-storySectionLink'})
             category = category_element.text.strip() if category_element else 'N/A'
@@ -31,8 +34,9 @@ for article in articles:
 # Assuming you want to write these to a CSV file
 csv_file_path = 'cbc_news_items.csv'  # Specify your desired file path
 
+# Open the file with 'w' mode to write the data
 with open(csv_file_path, mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
+    writer = csv.writer(file, escapechar='\\', quoting=csv.QUOTE_ALL)  # Set escape character and quote all fields
     writer.writerow(['Title', 'Category', 'Summary', 'Image Link', 'Article Link'])
     writer.writerows(news)
 
